f60d1fb32272659a3954b7c3d685372e2a89a519
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 08:13:05 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,5 +2,13 @@
 class Cat
 attr_reader "Maru"
 attr_writer "Maru"
+maru = Cat.new
+maru.name = "Maru"
+maru.meow
+end
+
+def meow
 
 end
+Use the attr_accessor macro to create a setter and getter method for a cat's name.
+Write a method, .meow, that outputs "meow!" to the terminal using the puts method when called on an instance of Cat.
